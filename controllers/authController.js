3c8d9a0e8b35e8da9136ce5bfdb36aefec05ff30
codexuz <142141576+codexuz@users.noreply.github.com>
2024-09-02 12:18:47 +0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,17 @@ const supabase = require('../config/database');
 
 // Register a new user
 exports.register = async (req, res) => {
-  const { email, password } = req.body;
-  const { data, error } = await supabase.auth.signUp({ email, password });
+  const { email, password, full_name } = req.body;
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        full_name,
+        avatar_url: 'https://bkxhwaeluswfwfxavmpt.supabase.co/storage/v1/object/public/avatars/avatar.jpg'
+      }
+    }
+  });
 
   if (error) return res.status(400).json({ error: error.message });
 
